f94b37e23dd52f20ecefd87514db522276217f33
Juanen <juanen77@gmail.com>
2016-05-08 22:44:32 +0200

    Add some test for aliment.

--- src/classes/aliment.js
+++ src/classes/aliment.js
@@ -1,5 +1,26 @@
-var aliment = function() {
+var aliment = function(id, name) {
+
+  constructor.apply(this, [id, name]);
+
+  function constructor(id, name) {
+    this._id = id;
+    this._name = name;
+    this._created = Math.floor(Date.now() / 1000);
+  }
+
+  this.getId = function() {
+    return this._id;
+  };
+
+  this.getName = function() {
+    return this._name;
+  };
+
+  this.getCreatedDate = function() {
+    return this._created;
+  };
+
   return this;
-}
+};
 
 module.exports = aliment;
--- test/aliment_test.js
+++ test/aliment_test.js
@@ -2,8 +2,30 @@ var assert = require('chai').assert;
 var Aliment = require('../src/classes/aliment.js');
 
 describe('Alimento', function() {
+
   it('Al instanciar devuelve un objeto', function () {
     var aliment = new Aliment();
     assert(typeof aliment == 'object');
   });
+
+  it('El alimento tiene un id numérico, y un nombre string', function() {
+    var id = 2;
+    var name = 'Calabacín';
+    var aliment = new Aliment(id, name);
+    assert.ok(aliment.getId());
+    assert.ok(aliment.getName());
+    assert(typeof aliment.getId() == 'number');
+    assert(typeof aliment.getName() == 'string');
+  });
+
+  it('El alimento tiene una fecha de creación válida', function() {
+    var id = 3;
+    var name = 'Tomate';
+    var created = Math.floor(Date.now() / 1000);
+    var aliment = new Aliment(id, name);
+    assert.ok(aliment.getCreatedDate());
+    assert(typeof aliment.getCreatedDate() == 'number');
+    assert.equal(aliment.getCreatedDate(), created);
+  });
+
 });
