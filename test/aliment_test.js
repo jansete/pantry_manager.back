fa3a55c61d3f693b1466918a74db2ae9a246e05e
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-05-13 11:01:11 +0200

    by jansete, nesimo: add a broken test for private access to variables

--- test/aliment_test.js
+++ test/aliment_test.js
@@ -27,6 +27,24 @@ describe('Alimento', function() {
     assert.equal(aliment._created, undefined);
   });
 
+  it('Las propiedades son únicas del objeto', function() {
+    var id_platano = 10;
+    var name_platano = 'Plátano';
+    var platano = new Aliment(id_platano, name_platano);
+
+    var id_naranja = 11;
+    var name_naranja = 'Naranja';
+    var naranja = new Aliment(id_naranja, name_naranja);
+
+    assert.equal(platano.getId(), id_platano);
+    assert.equal(platano.getName(), name_platano);
+
+    assert.equal(naranja.getId(), id_naranja);
+    assert.equal(naranja.getName(), name_naranja);
+  });
+
+
+
   it('El alimento tiene una fecha de creación válida', function() {
     var id = 3;
     var name = 'Tomate';
