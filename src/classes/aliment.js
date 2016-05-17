a2f57681cdc7dfa29db69d19388cebcc926ff729
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-05-17 19:24:07 +0200

    by jansete, nesimo: Aliment propierties are privates

--- src/classes/aliment.js
+++ src/classes/aliment.js
@@ -1,26 +1,27 @@
-var aliment = (function() {
-  var _id, _name, _created;
+var privateProps = new WeakMap();
 
+var aliment = (function() {
   function constructor(id, name) {
-    _id = id;
-    _name = name;
-    _created = Math.floor(Date.now() / 1000);
+      privateProps.set(this, {
+            _id: id,
+            _name: name,
+            _created: Math.floor(Date.now() / 1000)
+      });
   };
 
   constructor.prototype.getId = function() {
-    return _id;
+    return privateProps.get(this)._id;
   };
 
   constructor.prototype.getName = function() {
-    return _name;
+    return privateProps.get(this)._name;
   };
 
   constructor.prototype.getCreatedDate = function() {
-    return _created;
+    return privateProps.get(this)._created;
   };
 
   return constructor;
 })();
  
-
 module.exports = aliment;
